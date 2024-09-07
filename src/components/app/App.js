01a2b863408aaac047f55dc7f3de78755efb0c42
Adam Troxell <adamtxl@hotmail.com>
2024-09-06 22:11:50 -0500

    Updated code to wrap provider at app entry, set up register inputs.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,21 +1,28 @@
 // src/components/app/App.js
+
 import * as React from 'react';
-import { Provider } from 'react-redux';
-import store from '../../redux/store';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AddToUserList from '../userList/addToUserList';
-
+import Register from '../auth/Register';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isAuthenticated = useSelector(state => state.users.isAuthenticated);
+
   return (
-    <Provider store={store}>
- <View style={styles.container}>
-      <Text>Your home page is loading enough to render this text! </Text>
+    <View style={styles.container}>
+      <Text>Your home page is loading enough to render this text!</Text>
       <StatusBar style="auto" />
-      <AddToUserList />
+      {isAuthenticated ? (
+        <>
+          <AddToUserList />
+          {/* Add other authenticated components here */}
+        </>
+      ) : (
+        <Register />
+      )}
     </View>
-    </Provider>
   );
 }
 
---
+++ src/components/auth/Register.js
@@ -0,0 +1,61 @@
+// src/components/auth/Register.js
+
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { createUser } from '../../redux/actions/userActions';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const dispatch = useDispatch();
+
+  const handleRegister = () => {
+    dispatch(createUser({ username, password, email }));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Username:</Text>
+      <TextInput
+        style={styles.input}
+        value={username}
+        onChangeText={setUsername}
+      />
+      <Text style={styles.label}>Password:</Text>
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Text style={styles.label}>Email:</Text>
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+      />
+      <Button title="Register" onPress={handleRegister} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  label: {
+    marginBottom: 5,
+    fontSize: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingLeft: 8,
+  },
+});
+
+export default Register;
